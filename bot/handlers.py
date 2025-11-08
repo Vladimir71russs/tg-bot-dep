baf65f4f2d1a6d14f5686f0fe6832b560a7f9758
Владимир <162006856+Vladimir71russs@users.noreply.github.com>
2025-11-08 19:52:45 +0300

    Update handlers.py

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,20 +1,18 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from bot.learning import start_learning, continue_learning, finish_learning, generate_answer_options
 from bot.utils import get_main_menu, get_main_menu_button, get_user
 from bot.models import add_word_to_db, delete_word_from_db, get_user_words
 from bot.state import user_states
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from asgiref.sync import sync_to_async
 from dict.models import Word
 from random import shuffle
 
+MAX_MESSAGE_LENGTH = 4096
 
-MAX_MESSAGE_LENGTH = 4096  # Максимальная длина сообщения Telegram
 
 def split_message(text, max_length=MAX_MESSAGE_LENGTH):
-    """Разбивает сообщение на части, если оно слишком длинное."""
-    return [text[i:i+max_length] for i in range(0, len(text), max_length)]
+    return [text[i:i + max_length] for i in range(0, len(text), max_length)]
 
 
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -22,39 +20,23 @@ async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     username = update.message.chat.username or f"User_{telegram_id}"
 
     user, created = await get_user(telegram_id, username)
-    if created:
-        # Приветственное сообщение для нового пользователя
-        await update.message.reply_text(
-            f"Привет, {username}! Добро пожаловать в обучающего бота! 📚\n\n"
-            "📖 **Этот бот поможет тебе улучшить знание английского языка.**\n\n"
-            "Ты можешь:\n"
-            "- Добавлять новые слова и их перевод в свой личный словарь.\n"
-            "- Просматривать свой словарь, чтобы не забывать выученные слова.\n"
-            "- Удалять ненужные слова.\n"
-            "- Начать тренировку, чтобы проверить свои знания.\n\n"
-            "🔽 Используй меню ниже, чтобы начать!",
-            reply_markup=get_main_menu()
-        )
-    else:
-        # Приветствие для уже зарегистрированного пользователя
-        await update.message.reply_text(
-            f"Привет, {username}! Добро пожаловать в обучающего бота! 📚\n\n"
-            "📖 **Этот бот поможет тебе улучшить знание английского языка.**\n\n"
-            "Ты можешь:\n"
-            "- Добавлять новые слова и их перевод в свой личный словарь.\n"
-            "- Просматривать свой словарь, чтобы не забывать выученные слова.\n"
-            "- Удалять ненужные слова.\n"
-            "- Начать тренировку, чтобы проверить свои знания.\n\n"
-            "🔽 Используй меню ниже, чтобы начать!",
-            reply_markup=get_main_menu())
+    text = (
+        f"Привет, {username}! Добро пожаловать в обучающего бота! 📚\n\n"
+        "📖 **Этот бот поможет тебе улучшить знание английского языка.**\n\n"
+        "Ты можешь:\n"
+        "- Добавлять новые слова и их перевод.\n"
+        "- Просматривать свой словарь.\n"
+        "- Удалять слова.\n"
+        "- Начать тренировку на английском или русском.\n\n"
+        "🔽 Используй меню ниже!"
+    )
+    await update.message.reply_text(text, reply_markup=get_main_menu())
 
 
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     query = update.callback_query
     await query.answer()
 
-    # Если нажата кнопка с ответом
     if query.data.startswith("answer:"):
         await continue_learning(update, context)
         return
@@ -64,12 +46,15 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.message.reply_text("Главное меню:", reply_markup=get_main_menu())
 
     elif query.data == "learn_words":
-        await learn_handler(update, context)
+        await learn_handler(update, context, reverse=False)
+
+    elif query.data == "learn_words_ru":
+        await learn_handler(update, context, reverse=True)
 
     elif query.data == "add_word":
         user_states[query.message.chat_id] = {"state": "adding"}
         categories_info = (
-            "Выберите категорию, указав соответствующую цифру:\n"
+            "Выберите категорию:\n"
             "1: существительные\n"
             "2: глаголы\n"
             "3: прилагательные\n"
@@ -77,92 +62,59 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "5: словосочетания\n"
             "6: новые слова\n"
         )
-        await query.message.reply_text(f"{categories_info} Введите слово в формате 'индекс категории - английское - русский - транскрипция':", reply_markup=get_main_menu_button())
+        await query.message.reply_text(
+            f"{categories_info} Введите слово в формате 'индекс категории - английское - русский - транскрипция':",
+            reply_markup=get_main_menu_button()
+        )
 
     elif query.data == "my_words":
         words = await get_user_words(query.message.chat_id)
-
-        # на случай, если по ошибке get_user_words вернёт (list,) — нормализуем (необязательно, но безопасно)
         if isinstance(words, tuple) and len(words) == 1 and isinstance(words[0], list):
             words = words[0]
-
         if words:
             word_list = "\n".join([
                 f"{w.english_word} - {w.russian_word}" + (
-                    f" - {w.transcription}" if w.transcription else "") + f" - {w.category}"
+                    f" - {w.transcription}" if w.transcription else ""
+                ) + f" - {w.category}"
                 for w in words
             ])
             total_words = len(words)
-
-            for part in split_message(f"Ваш словарь:\n{word_list}\n\n📊 Всего слов в словаре: {total_words}"):
+            for part in split_message(f"Ваш словарь:\n{word_list}\n\n📊 Всего слов: {total_words}"):
                 await query.message.reply_text(part, reply_markup=get_main_menu())
         else:
             await query.message.reply_text("Ваш словарь пока пуст.")
 
-
     elif query.data == "delete_word":
         user_states[query.message.chat_id] = {"state": "deleting"}
-        await query.message.reply_text("Введите слово для удаления (на английском или русском):")
-
-
+        await query.message.reply_text("Введите слово для удаления:")
 
     elif query.data == "finish_learning":
-
-        # Подтверждение завершения обучения
-
         keyboard = [
-
             [
-
                 InlineKeyboardButton("✅ Закончить", callback_data="confirm_finish_learning"),
-
-                InlineKeyboardButton("🔁 Продолжить обучение", callback_data="continue_learning")
-
+                InlineKeyboardButton("🔁 Продолжить", callback_data="continue_learning"),
             ]
-
         ]
-
-        reply_markup = InlineKeyboardMarkup(keyboard)
-
         await query.message.reply_text(
-
             "Вы действительно хотите закончить обучение?",
-
-            reply_markup=reply_markup
-
+            reply_markup=InlineKeyboardMarkup(keyboard)
         )
 
-
     elif query.data == "confirm_finish_learning":
-
         if not user_states.get(query.message.chat_id):
             await query.message.reply_text(
-
-                "Сессия обучения не найдена, попробуйте начать сначала.",
-
-                reply_markup=get_main_menu_button()
-
+                "Сессия обучения не найдена.", reply_markup=get_main_menu_button()
             )
-
             return
-
         await finish_learning(update, context)
 
-
     elif query.data == "continue_learning":
-
         await query.message.reply_text("Продолжаем обучение 💪")
-
         await continue_learning(update, context)
-
 
     elif query.data == "edit_category":
         user_states[query.message.chat_id] = {"state": "editing"}
-        await query.message.reply_text(
-            "Введите слово, для которого хотите изменить категорию (на английском или русском):"
-        )
-
-
+        await query.message.reply_text("Введите слово для изменения категории:")
 
     else:
         await query.message.reply_text("Неизвестная команда.")
@@ -172,85 +124,23 @@ async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     telegram_id = update.message.chat_id
     text = update.message.text.strip()
 
-    # Если пользователь в режиме обучения
     if telegram_id in user_states and user_states[telegram_id].get("state") == "learning":
         await continue_learning(update, context)
         return
 
-    # Если пользователь в режиме удаления слов
     if telegram_id in user_states and user_states[telegram_id].get("state") == "deleting":
         await delete_word_from_db(telegram_id, text, update)
-        user_states.pop(telegram_id, None)  # Сбрасываем состояние
-        return
-
-    # Если пользователь в режиме добавления слова
-    if telegram_id in user_states and user_states[telegram_id].get("state") == "adding":
-        await add_word_to_db(telegram_id, text, update)
-        user_states.pop(telegram_id, None)  # Сбрасываем состояние
-        return
-
-
-
-    # Изменение категории
-    if telegram_id in user_states and user_states[telegram_id].get("state") == "editing":
-        user_states[telegram_id]["word_text"] = text
-        user_states[telegram_id]["state"] = "editing_category_choice"
-        category_text = (
-            "Введите новый номер категории:\n"
-            "1: существительные\n"
-            "2: глаголы\n"
-            "3: прилагательные\n"
-            "4: частицы\n"
-            "5: словосочетания\n"
-            "6: новые слова\n"
-        )
-        await update.message.reply_text(category_text)
-        return
-
-    # Получение новой категории
-    if telegram_id in user_states and user_states[telegram_id].get("state") == "editing_category_choice":
-        word_text = user_states[telegram_id].get("word_text")
-        new_category_index = text.strip()
-        category_map = {
-            "1": "существительные",
-            "2": "глаголы",
-            "3": "прилагательные",
-            "4": "частицы",
-            "5": "словосочетания",
-            "6": "новые слова",
-        }
-
-        if new_category_index not in category_map:
-            await update.message.reply_text("Неверный номер категории. Попробуйте снова.")
-            return
-
-        new_category = category_map[new_category_index]
-
-        # Обновляем категорию
-        word = await sync_to_async(Word.objects.filter)(
-            user__telegram_id=telegram_id
-        )
-        word = await sync_to_async(list)(word)
-        word = next((w for w in word if w.english_word.lower() == word_text.lower() or w.russian_word.lower() == word_text.lower()), None)
-
-        if not word:
-            await update.message.reply_text("Слово не найдено.")
-        else:
-            word.category = new_category
-            await sync_to_async(word.save)()
-            await update.message.reply_text(f"Категория слова '{word.english_word}' успешно изменена на '{new_category}' ✅",reply_markup=get_main_menu())
-
         user_states.pop(telegram_id, None)
         return
 
-    # Если пользователь вводит текст в неизвестном формате
-    await update.message.reply_text(
-        "Некорректный формат. Выберите действие из меню.",
-        reply_markup=get_main_menu()
-    )
+    if telegram_id in user_states and user_states[telegram_id].get("state") == "adding":
+        await add_word_to_db(telegram_id, text, update)
+        user_states.pop(telegram_id, None)
+        return
+
+    await update.message.reply_text("Некорректный формат.", reply_markup=get_main_menu())
 
 
-# Генерация меню с категориями
 def get_category_menu():
     keyboard = [
         [InlineKeyboardButton("Существительные", callback_data="category:существительные")],
@@ -264,35 +154,34 @@ def get_category_menu():
     return InlineKeyboardMarkup(keyboard)
 
 
-# Обработчик команды "учиться"
-async def learn_handler(update, context):
+async def learn_handler(update, context, reverse=False):
     if update.callback_query:
         await update.callback_query.message.reply_text(
             "Выберите категорию для обучения:",
             reply_markup=get_category_menu()
         )
-    elif update.message:
+        telegram_id = update.callback_query.from_user.id
+    else:
         await update.message.reply_text(
             "Выберите категорию для обучения:",
             reply_markup=get_category_menu()
         )
+        telegram_id = update.message.chat_id
+
+    user_states[telegram_id] = {"reverse": reverse}
 
 
-# Обработчик нажатия кнопок категорий
 async def category_handler(update, context):
     query = update.callback_query
     await query.answer()
 
-    # Получаем выбранную категорию
     callback_data = query.data
     if callback_data.startswith("category:"):
         category = callback_data.split(":", 1)[1]
-
         telegram_id = query.from_user.id
-        user_words = await sync_to_async(list)(
-            Word.objects.filter(user__telegram_id=telegram_id)
-        )
+        reverse_mode = user_states.get(telegram_id, {}).get("reverse", False)
 
+        user_words = await sync_to_async(list)(Word.objects.filter(user__telegram_id=telegram_id))
         if category != "все":
             user_words = [word for word in user_words if word.category == category]
 
@@ -303,32 +192,27 @@ async def category_handler(update, context):
         shuffle(user_words)
         user_states[telegram_id] = {
             "state": "learning",
-            "words": user_words,  # Список без изменений
+            "reverse": reverse_mode,
+            "words": user_words,
             "correct": 0,
             "incorrect": 0,
             "incorrect_pairs": [],
         }
 
-        # Берем первое слово, но НЕ удаляем его из списка
         current_word = user_words[0]
         user_states[telegram_id]["current_word"] = current_word
+        random_answers = await generate_answer_options(current_word, reverse=reverse_mode)
 
+        if reverse_mode:
+            message = f"Как переводится слово '{current_word.russian_word}'?"
+        else:
+            message = f"Как переводится слово '{current_word.english_word}'?"
 
-
-        # Генерируем варианты ответа
-        random_answers = await generate_answer_options(current_word)
-
-
-        message = f"Как переводится слово '{current_word.english_word}'?"
-        if current_word.transcription:
+        if current_word.transcription and not reverse_mode:
             message += f" [{current_word.transcription}]"
 
         keyboard = [
             [InlineKeyboardButton(answer, callback_data=f"answer:{answer}") for answer in random_answers],
             [InlineKeyboardButton("Закончить обучение", callback_data="finish_learning")],
         ]
-
-        await query.edit_message_text(
-            message,
-            reply_markup=InlineKeyboardMarkup(keyboard),
-        )
+        await query.edit_message_text(message, reply_markup=InlineKeyboardMarkup(keyboard))
